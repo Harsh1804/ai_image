664136435d56f0ca74f8cb70d8c6ed8551274e90
Harsh1804 <harshchhindre9718@gmail.com>
2023-06-15 00:57:38 +0530

    new_frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-import { logo } from './assets';
+import { logov2 } from './assets';
 import { Home, CreatePost } from './page';
 
 const App = () => (
   <BrowserRouter>
-    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-6 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img src={logov2} alt="logo" className="w-40 object-contain" />
       </Link>
+      <nav className="">
+      <Link to="/" className="font-inter font-medium px-8">Documentation</Link>
+      <Link to="/" className="font-inter font-medium px-8">Report</Link>
+      <Link to="/" className="font-inter font-medium px-8">About us</Link>
+      </nav>
+      
 
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+      
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,12 @@
 import download from './download.png';
 import logo from './logo.svg';
+import logov2 from './logov2.svg';
 import preview from './preview.png';
+
 
 export {
   download,
   logo,
+  logov2,
   preview,
 };
